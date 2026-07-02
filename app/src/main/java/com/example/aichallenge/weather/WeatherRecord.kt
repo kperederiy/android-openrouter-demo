0ce9b779fedf65417bd926f62bd48ec8858b374a
kperederiy <kperederiy@yandex.ru>
2026-07-02 20:21:20 +0300

    День 21. Индексация документов

--- app/src/main/java/com/example/aichallenge/weather/WeatherRecord.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aichallenge.weather
-
-data class WeatherRecord(
-    val timestamp: String,
-    val temperature: Double,
-    val condition: String,
-    val humidity: Int,
-    val windSpeed: Double
-)
